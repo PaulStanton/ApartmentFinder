940fccbc38c349463fa7cad972ec92c01d70b224
Stephen Kirkland <sk01417@uga.edu>
2017-03-03 13:38:46 -0500

    created MVC for ApartmentFinder

---
+++ ApartmentFinderMVC/Controllers/ApartmentsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ApartmentFinderDAL;
+
+namespace ApartmentFinderMVC.Controllers
+{
+    public class ApartmentsController : Controller
+    {
+        private ApartmentFinderEntities db = new ApartmentFinderEntities();
+
+        // GET: Apartments
+        public async Task<ActionResult> Index()
+        {
+            var apartments = db.Apartments.Include(a => a.City);
+            return View(await apartments.ToListAsync());
+        }
+
+        // GET: Apartments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = await db.Apartments.FindAsync(id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(apartment);
+        }
+
+        // GET: Apartments/Create
+        public ActionResult Create()
+        {
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "Name");
+            return View();
+        }
+
+        // POST: Apartments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ApartmentID,ApartmentName,Street_Address,ContactEmail,ContactPhone,CityID,Active")] Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Apartments.Add(apartment);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "Name", apartment.CityID);
+            return View(apartment);
+        }
+
+        // GET: Apartments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = await db.Apartments.FindAsync(id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "Name", apartment.CityID);
+            return View(apartment);
+        }
+
+        // POST: Apartments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ApartmentID,ApartmentName,Street_Address,ContactEmail,ContactPhone,CityID,Active")] Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(apartment).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "Name", apartment.CityID);
+            return View(apartment);
+        }
+
+        // GET: Apartments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = await db.Apartments.FindAsync(id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(apartment);
+        }
+
+        // POST: Apartments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Apartment apartment = await db.Apartments.FindAsync(id);
+            db.Apartments.Remove(apartment);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
