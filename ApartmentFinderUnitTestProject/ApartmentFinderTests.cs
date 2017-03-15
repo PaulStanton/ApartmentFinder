92229be3249505ab4bbce253a7ea7bcb231ad86e
Stephen Kirkland <sk01417@uga.edu>
2017-03-15 14:39:57 -0400

    Added Unit Testing

--- ApartmentFinderUnitTestProject/ApartmentFinderTests.cs
+++ ApartmentFinderUnitTestProject/ApartmentFinderTests.cs
@@ -8,23 +8,62 @@ namespace ApartmentFinderUnitTestProject
     [TestClass]
     public class ApartmentFinderTests
     {
-        Address address = new Address();
+        Address address = new Address("312 Riverbend Pkwy", "Athens", "30605", "Georgia");
+
+        Apartment apartment = new Apartment(3, "Surrey Square", "greensproperties@gmail.com", "706-555-5555", "b", new Address(), null);
 
         [TestMethod]
-        public void City()
+        public void ApartmentRd()
         {
-            address.Street_Address = "312 Riverbend Pkwy";
-            address.City = "Athens";
-            address.State = "Georgia";
-            address.Zip = "30605";
-
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(address.Street_Address, "312 Riverbend Pkwy");
         }
 
-        [Test]
-        public void Test()
+        [TestMethod]
+        public void CorrectApartmentStateWithNotEqual()
         {
-            
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(address.State, "Georgia");
+        }
+
+        [TestMethod]
+        public void CorrectApartmentStateWithEqual()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(address.State, "Georgia");
+        }
+
+        [TestMethod]
+        public void ApartmentZip()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(address.Zip, "30605");
+        }
+
+        [TestMethod]
+        public void FalseApartmentCity()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(address.City, "Brooklyn");
+        }
+
+        [TestMethod]
+        public void CorrectApartmentCity()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(address.City, "Athens");
+        }
+
+        [TestMethod]
+        public void CorrectApartmentName()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(apartment.ApartmentName, "Surrey Square");
+        }
+
+        [TestMethod]
+        public void CorrectApartmentNameSetToNotEqual()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(apartment.ApartmentName, "Surrey Square");
+        }
+
+        [TestMethod]
+        public void IncorrectApartmentName()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(apartment.ApartmentName, "The Slums");
         }
     }
 }
